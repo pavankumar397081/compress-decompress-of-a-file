a27478d5629428cfba25d6f02743b9fbea905ea8
pavankumar397081 <121242885+pavankumar397081@users.noreply.github.com>
2022-12-23 09:52:51 +0530

    Add files via upload

---
+++ view.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import CodeFiles.Compress;
+import CodeFiles.Decompress;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author yash
+ */
+public class view extends JFrame implements ActionListener{
+    
+    JButton compressButton;
+    JButton decompressButton;
+    
+    view(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        
+        compressButton = new JButton("Select File to Compress");
+        compressButton.addActionListener(this);
+        compressButton.setBounds(20, 200, 200, 30);
+        
+        decompressButton = new JButton("Select File to Decompress");
+        decompressButton.addActionListener(this);
+        decompressButton.setBounds(250, 200, 200, 30);
+        
+        this.add(compressButton);
+        this.add(decompressButton);
+        
+        this.setSize(500,300);
+        this.getContentPane().setBackground(Color.darkGray);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == compressButton){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+            
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+                try{
+                    Compress.compressFunction(file);
+                }
+                catch(Exception excp){
+                    JOptionPane.showMessageDialog(null, excp.toString());
+                }
+            }
+        }
+        
+        if(e.getSource() == decompressButton){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+            
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+                try{
+                    Decompress.decompressFunction(file);
+                }
+                catch(Exception excp){
+                    JOptionPane.showMessageDialog(null, excp.toString());
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        view view = new view();
+        view.setVisible(true);
+    }
+}
